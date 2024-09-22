905612e61f3379aa2f977dc66ad20901e884fbca
Vishwanath Maralappanavar - SKIDATA <vishwanath.maralappanavar@skidata.com>
2024-09-22 12:30:05 +0530

    updated the frontend file

--- mern/frontend/src/components/Record.jsx
+++ mern/frontend/src/components/Record.jsx
@@ -17,7 +17,8 @@ export default function Record() {
       if(!id) return;
       setIsNew(false);
       const response = await fetch(
-        `http://localhost:5050/record/${params.id.toString()}`
+        `http://13.201.26.28:5050/record/${params.id.toString()}`
+        //`http://localhost:5050/record/${params.id.toString()}`
       );
       if (!response.ok) {
         const message = `An error has occurred: ${response.statusText}`;
@@ -51,7 +52,9 @@ export default function Record() {
       let response;
       if (isNew) {
         // if we are adding a new record we will POST to /record.
-        response = await fetch("http://localhost:5050/record", {
+        response = await fetch("http://13.201.26.28:5050/record", 
+          //response = await fetch("http://localhost:5050/record",
+          {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -60,7 +63,9 @@ export default function Record() {
         });
       } else {
         // if we are updating a record we will PATCH to /record/:id.
-        response = await fetch(`http://localhost:5050/record/${params.id}`, {
+        response = await fetch(`http://13.201.26.28:5050/record/${params.id}`,
+          //response = await fetch(`http://localhost:5050/record/${params.id}`,
+          {
           method: "PATCH",
           headers: {
             "Content-Type": "application/json",
